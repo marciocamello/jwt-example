ac0207b7a46a2afbe8842590b5763ce2134e5f62
Peter van Meijgaard <petervanmeijgaard@hotmail.com>
2016-08-11 15:35:08 +0200

    Fix merge conflict

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
 <!DOCTYPE html><html><head><meta charset=utf-8><title>jwt-example</title><meta name=viewport content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=0"><link href=/static/css/app.a89ff748f8dd35caa56188959510cd62.css rel=stylesheet></head><body><div id=app></div><script type=text/javascript src=/static/js/manifest.7fdbb4e22a649a65229f.js></script><script type=text/javascript src=/static/js/vendor.0ede59f0826de75d13cb.js></script><script type=text/javascript src=/static/js/app.594a5a79ee9cbc6ef535.js></script></body></html>
-=======
-<!DOCTYPE html><html><head><meta charset=utf-8><title>jwt-example</title><meta name=viewport content="width=device-width,initial-scale=1,maximum-scale=1,user-scalable=0"><link href=/static/css/app.a89ff748f8dd35caa56188959510cd62.css rel=stylesheet></head><body><div id=app></div><script type=text/javascript src=/static/js/manifest.3c86f8fbe2dd27fe3b03.js></script><script type=text/javascript src=/static/js/vendor.9fdf34316b34853776ec.js></script><script type=text/javascript src=/static/js/app.ebaecb8cae78af13b7c1.js></script></body></html>
->>>>>>> master
