c02690dff57fae186f5d621442b95de602ea1e08
Peter van Meijgaard <petervanmeijgaard@hotmail.com>
2016-08-13 15:54:54 +0200

    Removed TestController and update the AccountTransformer

--- app/Transformers/AccountTransformer.php
+++ app/Transformers/AccountTransformer.php
@@ -8,15 +8,9 @@ class AccountTransformer extends Transformer
     public function transform($user)
     {
         return [
-            'id'        => (int) $user->id,
-            'email'     => $user->email,
-            'name'      => $user->name,
-            'mobile'    => $user->mobile_phone,
-            'telephone' => $user->telephone,
-            'birthday'  => $user->birthday ? $user->birthday->toDateString() : null,
-            'gender'    => $user->sex,
-            'bio'       => $user->bio,
-            'avatar'    => $user->avatar
+            'id'    => (int) $user->id,
+            'email' => $user->email,
+            'name'  => $user->name
         ];
     }
 }
